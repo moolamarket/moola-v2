adeecf1462e618052adf4d837e314d7f9636a6b8
ytumanov <airhole.ua@gmail.com>
2022-01-05 15:34:55 +0200

    transfer swapped to initiator

--- contracts/adapters/UniswapLiquiditySwapAdapter.sol
+++ contracts/adapters/UniswapLiquiditySwapAdapter.sol
@@ -247,6 +247,8 @@ contract UniswapLiquiditySwapAdapter is BaseUniswapAdapter {
       IERC20(assetTo).safeApprove(address(LENDING_POOL), 0);
       IERC20(assetTo).safeApprove(address(LENDING_POOL), vars.receivedAmount);
       LENDING_POOL.deposit(assetTo, vars.receivedAmount, initiator, 0);
+    } else {
+      IERC20(assetTo).transfer(initiator, vars.receivedAmount);
     }
 
     vars.flashLoanDebt = amount.add(premium);
