c5a5fbd33989487878569d8f20a5e7deeaae1ba0
x-moola <x@moola.market>
2022-02-24 16:36:08 -0800

    Update addresses in MOOConfigurator.sol

--- contracts/protocol/lendingpool/MOO/MOOConfigurator.sol
+++ contracts/protocol/lendingpool/MOO/MOOConfigurator.sol
@@ -16,11 +16,11 @@ contract MOOConfigurator is Ownable {
   bytes constant params = '0x10';
   bool constant stableBorrowRateEnabled = true;
   uint8 constant underlyingAssetDecimals = 18;
-  address constant aTokenImpl = 0xe8B286649713447D8d5fBeBC28c731830d19B6C9;
-  address constant stableDebtTokenImpl = 0xB6a5059A228a16Fa2827E28E52ceC96BBC63D639;
-  address constant variableDebtTokenImpl = 0xB65b6a6a6F78E4daABF259c756567ae346699687;
-  address constant interestRateStrategyAddress = 0x5B41b0c78659636c6664f08F7cCb620ceA3F1206;
-  address constant treasury = 0x643C574128c7C56A1835e021Ad0EcC2592E72624;
+  address constant aTokenImpl = 0x55bFCED2451b2154e06604D4269c9349F31141e6;
+  address constant stableDebtTokenImpl = 0xaCdb7B3e2b0a038F1f4eF04736728E0065b689DA;
+  address constant variableDebtTokenImpl = 0x0301Cf8F1FCD9255BD32FB7e0fE5B3494f445C2C;
+  address constant interestRateStrategyAddress = 0xb3072f5F0d5e8B9036aEC29F37baB70E86EA0018;
+  address constant treasury = 0x313bc86D3D6e86ba164B2B451cB0D9CfA7943e5c;
   address constant incentivesController = 0x0000000000000000000000000000000000000000;
   string constant underlyingAssetName = 'Moola';
   string constant aTokenName = 'Moola interest bearing MOO';
