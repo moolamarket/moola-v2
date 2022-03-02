a1dcf20366c30563e4aacb73afa552f8e890f5c3
x-moola <x@moola.market>
2022-03-02 07:10:43 -0800

    Transfer token from msg.sender to the helper contract

--- contracts/protocol/lendingpool/RepaymentHelper.sol
+++ contracts/protocol/lendingpool/RepaymentHelper.sol
@@ -2,6 +2,7 @@ pragma solidity 0.6.12;
 
 import {ILendingPoolAddressesProvider} from '../interfaces/ILendingPoolAddressesProvider.sol';
 import {ILendingPool} from '../interfaces/ILendingPool.sol';
+import {IERC20} from '../../dependencies/openzeppelin/contracts/IERC20.sol';
 
 contract RepaymentHelper {
   event DelegateRepay(
@@ -18,12 +19,21 @@ contract RepaymentHelper {
     ADDRESSES_PROVIDER = addressesProvider;
   }
 
+  /**
+   * @notice Transfer token from msg.sender to itself, repays the debt of _delegatee, and deposit the remaining to _delegatee if the repaid amount is less than _amount
+   * @param _delegatee The wallet address to repay debt of
+   * @param _asset The asset address to repay
+   * @param _amount The amount to repay
+   * @param _rateMode The rateMode to use for repayment
+   */
   function DelegateRepayHelper(
     address _delegatee,
     address _asset,
     uint256 _amount,
     uint256 _rateMode
   ) external {
+    IERC20(_asset).transferFrom(msg.sender, address(this), _amount);
+
     ILendingPool lendingPool = ILendingPool(ADDRESSES_PROVIDER.getLendingPool());
     uint256 paybackAmount = lendingPool.repay(_asset, _amount, _rateMode, _delegatee);
 
